6df874909082053a271b78eef607548964304eed
viresh18 <viresh.renukoot@gmail.com>
2018-06-30 19:33:02 +0530

    Added base and index html file, created views for index

--- src/simplesocial/simplesocial/urls.py
+++ src/simplesocial/simplesocial/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.HomePage.as_view(),name='home')
 ]
